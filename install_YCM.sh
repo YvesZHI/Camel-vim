24aaf3beaa047b341afc09af94dc585ce770ce75
Yves <yuhui.zhi01@gmail.com>
2021-04-07 15:53:00 +0800

    Update install_YCM.sh

--- install_YCM.sh
+++ install_YCM.sh
@@ -7,4 +7,5 @@ cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive
 #cd ~/.vim/bundle/YouCompleteMe/third_party/ycmd/third_party/go/src/golang.org/x/ && git clone https://github.com/golang/tools.git
 #cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive
 
+#CXX=clang++-8 ./install.py --clangd-completer
 ~/.vim/bundle/YouCompleteMe/install.py --clangd-completer
